838bfca4c7f138b6433e519286295ef6cfdba420
MasterofMakros <128098624+MasterofMakros@users.noreply.github.com>
2026-03-11 14:38:48 +0100

    fix(ui): restore relative z-Index to MagicCard to unhide internal content

--- components/ui/magic-card.tsx
+++ components/ui/magic-card.tsx
@@ -102,7 +102,7 @@ export function MagicCard({
           opacity: gradientOpacity,
         }}
       />
-      <div className="h-full w-full flex-1 flex flex-col z-10">{children}</div>
+      <div className="relative z-10 w-full flex-1 flex flex-col">{children}</div>
     </div>
   )
 }
